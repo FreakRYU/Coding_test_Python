e644e04a4ce26243a7f8b8e1aa8b6fd6b2fe59c6
류민서 <111236791+FreakRYU@users.noreply.github.com>
2023-06-25 23:13:14 +0900

    [Silver IV] Title: 스택, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,28 @@
+import sys
+
+n = int(sys.stdin.readline())
+stack = []
+
+for _ in range(n):
+    command = sys.stdin.readline().split()
+
+    if command[0] == 'push':
+        stack.append(int(command[1]))
+    elif command[0] == 'pop':
+        if stack:
+            print(stack.pop())
+        else:
+            print(-1)
+    elif command[0] == 'size':
+        print(len(stack))
+    elif command[0] == 'empty':
+        if stack:
+            print(0)
+        else:
+            print(1)
+    elif command[0] == 'top':
+        if stack:
+            print(stack[-1])
+        else:
+            print(-1)
+   
